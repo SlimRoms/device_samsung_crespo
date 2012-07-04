3a5f1c962ccc0dad9eb15393228d3204f6592974
Jean-Baptiste Queru <jbq@google.com>
2012-07-04 11:33:06 -0700

    update self-extractors
    Change-Id: Iebd547a94abc3115809393e02a711ca70828c767

--- self-extractors/generate-packages.sh
+++ self-extractors/generate-packages.sh
@@ -41,6 +41,7 @@
 # end ics-mr1
 # start jb-dev
 # 385121 = JRN79
+# 397816 = JRO03B
 # end jb-dev
 BRANCH=jb-dev
 if test $BRANCH=ics-mr1
@@ -50,8 +51,8 @@ then
 fi # ics-mr1
 if test $BRANCH=jb-dev
 then
-  ZIP=soju-ota-385121.zip
-  BUILD=jrn79
+  ZIP=soju-ota-397816.zip
+  BUILD=jro03b
 fi # jb-dev
 ROOTDEVICE=crespo
 DEVICE=crespo
