9dfc9e4441457040765835eb94db7ced0bddb7e9
Tarak <tarak.chaari@gmail.com>
2012-10-07 20:50:29 +0100

    Vsync disabled and slim.dependencies fixed

--- slim.mk
+++ slim.mk
@@ -37,7 +37,7 @@ PRODUCT_COPY_FILES += \
 # Release name
 PRODUCT_RELEASE_NAME := NS
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_FINGERPRINT="google/soju/crespo:4.1.1/JRO03L/113740:user/release-keys" 
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_FINGERPRINT="google/soju/crespo:4.1.1/JRO03R/113740:user/release-keys" 
 PRIVATE_BUILD_DESC="soju-user 4.1.1 JRO03R 113740 release-keys"
 
 PRODUCT_NAME := slim_crespo
